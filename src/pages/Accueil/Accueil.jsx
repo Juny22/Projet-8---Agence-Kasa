63303b61d67048d7e304e473efb6babd4b344948
Juny22 <127967096+Juny22@users.noreply.github.com>
2023-09-14 22:29:55 +0200

    First commit

---
+++ src/pages/Accueil/Accueil.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Accueil.css";
+
+function Accueil() {
+    return(
+        <div >
+            Chez vous, partout et ailleurs
+        </div>
+    );
+}
+
+export default Accueil;

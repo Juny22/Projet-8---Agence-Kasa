7ee6218a49f51800e8c31358f2916ae5892598af
Juny22 <127967096+Juny22@users.noreply.github.com>
2023-09-15 10:56:02 +0200

    Routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from 'react-router-dom'
 import Layout from "../Layouts/Layouts"
 import Accueil from "../pages/Accueil/Accueil";
 import Fiche from '../pages/Fiche-logement/Fiche-logement';
@@ -9,17 +9,14 @@ import Erreur from "../pages/Error/Error";
 
 function RoutesPath() {
     return(
-        <HashRouter>
             <Layout>
                 <Routes>
-                    <Route element={<Navigate replace to="/accueil" />} path="/" />
                     <Route path="/accueil" element={<Accueil />}/>
                     <Route path="/logement/:id" element={<Fiche />}/>
                     <Route path="/a-propos" element={<APropos />}/>
                     <Route path="*" element={<Erreur />}/>
                 </Routes>
             </Layout>
-        </HashRouter>
     );
 }
 

7133e7f708460d6ff9e8390b2dab41dfd1dea1b8
Lang Ya <lang.ya351@gmail.com>
2023-12-04 18:07:36 +0100

    update routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,9 +7,8 @@ import Erreur from "../pages/Error/Error";
 
 function RoutesPath() {
   const genericLoader = async ({ params }) => {
-    const { id } = params; // Extrayez l'ID du paramètre d'URL
+    const { id } = params;
     
-    // Chargez les données spécifiques au logement avec l'ID donné
     const req = await fetch(`./logements.json?id=${id}`);
     const res = await req.json();
     
@@ -18,25 +17,25 @@ function RoutesPath() {
 
   const router = createBrowserRouter([
     {
-      path: "/Projet-8---Agence-Kasa/",
+      path: "/",
       loader: genericLoader,
       element: <Accueil />,
       errorElement: <Erreur />
     },
     {
-      path: "/Projet-8---Agence-Kasa/accueil",
+      path: "/accueil",
       loader: genericLoader,
       element: <Accueil />,
       errorElement: <Erreur />
     },
     {
-      path: "/Projet-8---Agence-Kasa/logement/:id",
+      path: "/logement/:id",
       loader: genericLoader,
       element: <FicheLogement />,
       errorElement: <Erreur />
     },
     {
-      path: "/Projet-8---Agence-Kasa/a-propos",
+      path: "/a-propos",
       loader: genericLoader,
       element: <APropos />,
       errorElement: <Erreur />
